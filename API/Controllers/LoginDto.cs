db6bae878b8165bfd100191ee36c9aa6463e1b18
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2024-11-27 09:43:20 +0200

    added login feature

---
+++ API/Controllers/LoginDto.cs
@@ -0,0 +1,7 @@
+namespace API.Controllers;
+
+public class LoginDto
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,28 +5,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    // api/users
-    public class UsersController : BaseApiController
+    public class UsersController(DataContext context) : BaseApiController
     {
-        private readonly DataContext _context;
-
-        public UsersController(DataContext context)
-        {
-            _context = context;
-        }
-        
-        [HttpGet]
+        [HttpGet] // users
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = await _context.Users.ToListAsync();
+            var users = await context.Users.ToListAsync();
             
             return users;
         }
         
-        [HttpGet("{id:int}")] // /api/users/2
+        [HttpGet("{id:int}")] // users/2
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var user = await context.Users.FindAsync(id);
             
             if(user == null) return NotFound();
             
