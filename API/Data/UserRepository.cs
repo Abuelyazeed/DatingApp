e02cc17a2ebc2960bf040d7151d58aa31551cd3d
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2024-12-01 08:59:29 +0200

    updated userController to use the repo instead of context

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -18,7 +18,9 @@ public class UserRepository(DataContext context) : IUserRepository
 
     public async Task<IEnumerable<AppUser>> GetUsersAsync()
     {
-        return await context.Users.ToListAsync();
+        return await context.Users
+            .Include(x => x.Photos)
+            .ToListAsync();
 
     }
 
@@ -29,6 +31,8 @@ public class UserRepository(DataContext context) : IUserRepository
 
     public async Task<AppUser?> GetUserByUsernameAsync(string username)
     {
-        return await context.Users.SingleOrDefaultAsync(u => u.UserName == username);
+        return await context.Users
+            .Include(x => x.Photos)
+            .SingleOrDefaultAsync(u => u.UserName == username);
     }
 }
