49a6e7b0cbda4f7ecf33536fc7d6944a0fd8f3aa
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2024-12-06 14:53:33 +0200

    added paginationHelper

--- API/Helpers/PagedLists.cs
+++ API/Helpers/PagedLists.cs
@@ -1,6 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Helpers;
 
-public class PagedLists
+public class PagedLists<T> : List<T>
 {
-    
+    public PagedLists(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+    {
+        CurrentPage = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        PageSize = pageSize;
+        TotalCount = count;
+        AddRange(items);
+    }
+
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+
+    public static async Task<PagedLists<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+    {
+        //Get total count of items in source, (source is the data that comes from the db query)
+        var count = await source.CountAsync();
+        
+        //Fetches only the items for the current page
+        //Skip((pageNumber - 1) * pageSize): Skips over the items from previous pages.
+        //For example, if you’re on page 2 with a page size of 10, it will skip the first 10 items.
+        //Take(pageSize): Selects the next pageSize items (e.g., 10 items).
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        
+        return new PagedLists<T>(items, count, pageNumber, pageSize);
+    }
 }
