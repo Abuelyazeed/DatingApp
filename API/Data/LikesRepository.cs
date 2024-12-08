91eb8122ae25174b94da1f8f3af90b8834b5a521
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2024-12-08 14:32:29 +0200

    created the likes repository

--- API/Data/LikesRepository.cs
+++ API/Data/LikesRepository.cs
@@ -1,38 +1,66 @@
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class LikesRepository : ILikesRepository
+public class LikesRepository(DataContext context, IMapper mapper) : ILikesRepository
 {
-    public Task<UserLike?> GetUserLike(int sourceId, int targetId)
+    public async Task<UserLike?> GetUserLike(int sourceId, int targetId)
     {
-        throw new NotImplementedException();
+        return await context.Likes.FindAsync(sourceId, targetId);
     }
 
-    public Task<IEnumerable<MemberDto>> GetUserLikes(string predicate, int userId)
+    public async Task<IEnumerable<MemberDto>> GetUserLikes(string predicate, int userId)
     {
-        throw new NotImplementedException();
+        var likes = context.Likes.AsQueryable();
+
+        switch (predicate)
+        {
+            case "liked":
+                return await likes.Where(x => x.SourceUserId == userId)
+                    .Select(u => u.TargetUser)
+                    .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                    .ToListAsync();
+            case "likedby":
+                return await likes.Where(x => x.TargetUserId == userId)
+                    .Select(u => u.SourceUser)
+                    .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                    .ToListAsync();
+            default: //get mutual likes
+                var likedIds = await GetCurrentUserLikeIds(userId);  //users the user likes
+
+                return await likes
+                    .Where(x => x.TargetUserId == userId && likedIds.Contains(x.SourceUserId))// gets mutual likes, likes user, and the user likes
+                    .Select(x => x.SourceUser)
+                    .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                    .ToListAsync();
+        }
     }
 
-    public Task<IEnumerable<int>> GetCurrentUserLikeIds(int userId)
+    public async Task<IEnumerable<int>> GetCurrentUserLikeIds(int userId)
     {
-        throw new NotImplementedException();
+        return await context.Likes
+            .Where(x => x.SourceUserId == userId)
+            .Select(x => x.TargetUserId)
+            .ToListAsync();
     }
 
-    public void DeleteLike(UserLike userLike)
+    public void DeleteLike(UserLike like)
     {
-        throw new NotImplementedException();
+        context.Likes.Remove(like);
     }
 
-    public void AddLike(UserLike userLike)
+    public void AddLike(UserLike like)
     {
-        throw new NotImplementedException();
+        context.Likes.Add(like);
     }
 
-    public Task<bool> SaveChanges()
+    public async Task<bool> SaveChanges()
     {
-        throw new NotImplementedException();
+        return await context.SaveChangesAsync() > 0;
     }
 }
--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -8,7 +8,7 @@ public interface ILikesRepository
     Task<UserLike?> GetUserLike(int sourceId, int targetId); //to check if one user has already liked another.
     Task<IEnumerable<MemberDto>> GetUserLikes(string predicate, int userId); //display all users liked by specific user and all users that like that specfic user
     Task<IEnumerable<int>> GetCurrentUserLikeIds(int userId); //get ids of the users the user has liked
-    void DeleteLike(UserLike userLike);
-    void AddLike(UserLike userLike);
+    void DeleteLike(UserLike like);
+    void AddLike(UserLike like);
     Task<bool> SaveChanges();
 }
