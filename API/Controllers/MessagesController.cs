fc09926f5c3c59d485d478142f6139dc6937d18c
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2024-12-14 16:47:14 +0200

    Finished the messaging feature

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -1,13 +1,16 @@
 using API.DTOs;
 using API.Extentions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Message = API.Entities.Message;
 
 namespace API.Controllers;
 
+[Authorize]
 public class MessagesController(IMessageRepository messageRepository, IUserRepository userRepository,
     IMapper mapper) : BaseApiController
 {
@@ -38,5 +41,52 @@ public class MessagesController(IMessageRepository messageRepository, IUserRepos
         if(await messageRepository.SaveAllAsync()) return Ok(mapper.Map<MessageDto>(message));
         
         return BadRequest("Failed to send message");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery]MessageParams messageParams)
+    {
+        messageParams.Username = User.getUsername();
+
+        var messages = await messageRepository.GetMessagesForUser(messageParams);
+        
+        Response.AddPaginationHeader(messages);
+        
+        return Ok(messages);
+    }
+
+    [HttpGet("thread/{username}")]
+    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesThread(string username)
+    {
+        var currentUsername = User.getUsername();
+        
+        var messages = await messageRepository.GetMessagesThread(currentUsername, username);
+        
+        return Ok(messages);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteMessage(int id)
+    {
+        var username = User.getUsername();
+        
+        var message = await messageRepository.GetMessage(id);
+        
+         if(message == null) return BadRequest("Cannot delete this message");
+         
+         if(message.SenderUsername != username || message.RecipientUsername != username)
+             return Forbid("You cannot delete this message");
+         
+         if(message.SenderUsername == username) message.SenderDeleted = true;
+         if(message.RecipientUsername == username) message.RecipientDeleted = true;
+
+         if (message is { SenderDeleted: true, RecipientDeleted: true })
+         {
+             messageRepository.DeleteMessage(message);
+         }
+
+         if (await messageRepository.SaveAllAsync()) return Ok();
+         
+         return BadRequest("Problem deleting message");
     }
 }
--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -2,10 +2,13 @@ using API.DTOs;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class MessageRepository(DataContext context) :IMessageRepository
+public class MessageRepository(DataContext context, IMapper mapper) :IMessageRepository
 {
     public void AddMessage(Message message)
     {
@@ -22,14 +25,46 @@ public class MessageRepository(DataContext context) :IMessageRepository
         return await context.Messages.FindAsync(id);
     }
 
-    public async Task<PagedList<MessageDto>> GetMessagesForUser()
+    public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
     {
-        throw new NotImplementedException();
+        var query = context.Messages.OrderByDescending(x => x.MessageSent).AsQueryable();
+
+        query = messageParams.Container switch
+        {
+            "Inbox" => query.Where(x => x.Recipient.UserName == messageParams.Username && x.RecipientDeleted == false), //Messages where the user is the recipient.
+            "Outbox" => query.Where(x => x.Sender.UserName == messageParams.Username && x.SenderDeleted == false), //Messages where the user is the sender.
+            _ => query.Where(x => x.Recipient.UserName == messageParams.Username && x.DateRead == null && x.RecipientDeleted == false), //Messages where the user is the recipient.(unread)
+        };
+
+        var messages = query.ProjectTo<MessageDto>(mapper.ConfigurationProvider);
+        
+        return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
     }
 
-    public async Task<IEnumerable<MessageDto>> GetMessagesThread(string currentUsername, string recepient)
+    public async Task<IEnumerable<MessageDto>> GetMessagesThread(string currentUsername, string recepientUsername)
     {
-        throw new NotImplementedException();
+        var messages = await context.Messages
+            .Include(x => x.Sender).ThenInclude(x =>x.Photos) //Join messages with sender, then with its photos
+            .Include(x => x.Recipient).ThenInclude(x =>x.Photos)//Join messages with recipient then with its photos
+            .Where(x => x.RecipientUsername == currentUsername
+                        && x.RecipientDeleted == false 
+                        && x.SenderUsername == recepientUsername || //Get messages sent to current user
+                        x.SenderUsername == currentUsername 
+                        && x.SenderDeleted == false 
+                        && x.RecipientUsername == recepientUsername) //Get messages sent by current user
+            .OrderBy(x => x.MessageSent)
+            .ToListAsync();
+        
+        var unreadMessages = messages.Where(x => x.DateRead == null && x.RecipientUsername == currentUsername)
+            .ToList(); //Get unread messages
+
+        if (unreadMessages != null)
+        {
+            unreadMessages.ForEach(x => x.DateRead = DateTime.UtcNow); //Mark unread messages as read
+            await context.SaveChangesAsync();
+        }
+
+        return mapper.Map<IEnumerable<MessageDto>>(messages);
     }
 
     public async Task<bool> SaveAllAsync()
